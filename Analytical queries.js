3285321465cf0e11cf37b481c380a0485b556d8f
DikshaGiria <57097322+DikshaGiria@users.noreply.github.com>
2020-12-06 19:02:40 +0530

    added analytical queries

---
+++ Analytical queries.js
@@ -0,0 +1,16 @@
+db.customers.find().limit(5);
+
+#first query
+db.customers.aggregate([{ $group : { _id : "$CustomerID", number : { $sum : 1 }}},{ $sort : { number: -1 } },{ $limit : 10 }]);
+    
+#second query
+db.customers.aggregate({ $group : { _id : "$Description", number : { $sum : 1 }}},{ $sort : { number: -1 } },{ $limit : 10 });
+
+#third query
+db.customers.aggregate( [{$match: {Country : 'France'}},
+                        { $group : { _id : "$CustomerID", number : { $sum : 1 }}},
+                        { $sort : { number: -1 } },
+                        { $limit : 1 }]);
+                       
+#Fourth query                     
+db.customers.distinct('InvoiceNo', {Country: 'France'}).length                       
